12d866976740061f0b8fabb4eec0c9de7aac3825
chloe0x0 <ChloeZeiter@gmail.com>
2023-10-04 23:00:03 -0700

    Init commit

---
+++ src/8p.py
@@ -0,0 +1,12 @@
+import discord
+import os 
+import dotenv
+
+TARGETS = 
+
+class Cwient(discord.Client):
+    def __init__():
+        super().__init__()
+    
+if __name__ == "__main__":
+    cwient = Cwient()
